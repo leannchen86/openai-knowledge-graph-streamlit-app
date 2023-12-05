b02924ca4b01fda9753cdd9a44367607fb0f9d90
Leann Chen <80327331+leannchen86@users.noreply.github.com>
2023-12-05 17:02:00 -0500

    Update App.py

--- App.py
+++ App.py
@@ -1,23 +1,47 @@
 import streamlit as st
+from langchain.chat_models import ChatOpenAI
+from langchain.graphs import Neo4jGraph
+from retrievers import typical_rag
+from chain import chain, Question
 from streamlit_agraph import agraph, Node, Edge, Config
 from neo4j import GraphDatabase
 import os
-
-os.environ["NEO4J_URI"] = st.secrets["NEO4J_URI"]
-os.environ["NEO4J_USERNAME"] =st.secrets["NEO4J_USERNAME"]
-os.environ["NEO4J_PASSWORD"] = st.secrets["NEO4J_PASSWORD"]
-
 import openai
 # from langchain.agents import initialize_agent
-from langchain.chat_models import ChatOpenAI
-from neo4j_advanced_rag.neo4j_vector import VectorTool
-from neo4j_advanced_rag.neo4j_cypher import GraphTool
-from neo4j_advanced_rag.history import get_graph_history, save_graph_history
-import os
-import importlib
+st.title("The OpenAI Saga")
 
-# Initialize the LLM
-llm = ChatOpenAI()
+NEO4J_URI= st.secrets["NEO4J_URI"]
+NEO4J_USERNAME= st.secrets["NEO4J_USERNAME"]
+NEO4J_PASSWORD= st.secrets["NEO4J_PASSWORD"]
+
+graph = Neo4jGraph(
+    url=NEO4J_URI,
+    username=NEO4J_USERNAME,
+    password=NEO4J_PASSWORD
+)
+
+with st.sidebar:
+    openai_api_key = st.text_input("OpenAI API Key", key="langchain_search_api_key_openai", type="password")
+    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+openai.api_key = openai_api_key
+
+# Initialize OpenAI API key and Chat model
+if openai_api_key:
+    model = ChatOpenAI(api_key=openai_api_key)
+
+# Function to process the query and return a response
+def process_query(query):
+    # Use GraphCypherQAChain to get a Cypher query and a natural language response
+    result = cypher_chain(query)
+    intermediate_steps = result['intermediate_steps']
+    final_answer = result['result']
+    generated_cypher = intermediate_steps[0]['query']
+    nl_response = final_answer
+    
+    # Fetch graph data using the Cypher query
+    nodes, edges = fetch_graph_data(direct_cypher_query=generated_cypher, intermediate_steps=intermediate_steps)
+    
+    return nl_response, visual, nodes, edges
 
 # Function to construct the Cypher query based on selected filters
 def construct_cypher_query(node_types, rel_types):
@@ -40,55 +64,41 @@ def construct_cypher_query(node_types, rel_types):
     
     return query
 
+def process_graph_result(context):
+    nodes = []
+    edges = []
+    node_names = set()  # This defines node_names to track unique nodes
+
+    for record in context:  # Adjusted to access 'Full Context' from the result
+        # Process nodes
+        p_name = record['p.name']
+        o_name = record['o.name']
+
+        # Add nodes if they don't already exist
+        if p_name not in node_names:
+            nodes.append(Node(id=p_name, label=p_name, size=5, shape="circle"))
+            node_names.add(p_name)
+        if o_name not in node_names:
+            nodes.append(Node(id=o_name, label=o_name, size=5, shape="circle"))
+            node_names.add(o_name)
+
+        # Process edges
+        relationship_label = record['type(r)']
+        edges.append(Edge(source=p_name, target=o_name, label=relationship_label))
+
+    return nodes, edges
+
 # Function to fetch data from Neo4j
-def fetch_graph_data(driver, nodesType, relType):
-    cypher_query = construct_cypher_query(nodesType, relType)
-    with driver.session() as session:
-        result = session.run(cypher_query)
-        nodes = []
-        edges = []
-        node_names = set()  # To keep track of unique node names
-
-        for record in result:
-            # Process nodes
-            p = record['p']
-            n = record['n']
-            p_name = p['name']
-            n_name = n['name']
-
-            # Add nodes if they don't already exist
-            if p_name not in node_names:
-                nodes.append(Node(id=p_name, label=p_name, size=25, shape="circle"))
-                node_names.add(p_name)
-
-            if n_name not in node_names:
-                nodes.append(Node(id=n_name, label=n_name, size=25, shape="circle"))
-                node_names.add(n_name)
-
-            # Process edges, include the date in the label if it exists
-            r = record['r']
-            relationship_label = r.type
-            if 'date' in r:
-                relationship_label = f"{r.type} ({r['date']})"
-            edges.append(Edge(source=p_name, target=n_name, label=relationship_label))
-
-        return nodes, edges
-
-with st.sidebar:
-    openai_api_key = st.text_input("OpenAI API Key", key="langchain_search_api_key_openai", type="password")
-    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
-openai.api_key = openai_api_key
-
-def get_neo4j_connection():
-    return GraphDatabase.driver(url, auth=(username, password))
-# Initialize the Neo4j connection
-driver = get_neo4j_connection()
-
-# Render the graph
-st.title('Neo4j Graph Visualization')
-# uri= st.secrets["uri"]
-# username= st.secrets["username"]
-# password= st.secrets["password"]
+def fetch_graph_data(nodesType=None, relType=None, direct_cypher_query=None, intermediate_steps=None):
+    # Use the direct Cypher query if provided
+    if direct_cypher_query:
+        cypher_query = direct_cypher_query
+    else:
+        # Construct the Cypher query based on selected filters
+        cypher_query = construct_cypher_query(nodesType, relType)
+    context = intermediate_steps[0]['context']
+    nodes, edges = process_graph_result(context)
+    return nodes, edges
 
 # Fetch the unique node types and relationship types for sidebar filters
 node_types = ['Person', 'Organization', 'Group', 'Topic']
@@ -100,10 +110,6 @@ relationship_types = [
     'FOUNDED_BY', 'INVESTED_IN', 'DEMOTED_FROM', 'RELEASES_HIRING_STATEMENT', 'HIRED_BY', 'REGRETS_FIRING','MENTIONS', 'EXPLAINS_DECISIONS', 'DESCRIBES', 'FORMER_PRESIDENT']
 
 st.sidebar.header('Filters')
-selected_node_types = st.sidebar.multiselect('Node Types', node_types, default=node_types)
-selected_relationship_types = st.sidebar.multiselect('Relationship Types', relationship_types, default=relationship_types)
-
-# Sidebar filters for node and relationship types
 selected_node_types = st.sidebar.multiselect('Node Types', node_types, default=node_types)
 selected_relationship_types = st.sidebar.multiselect('Relationship Types', relationship_types, default=relationship_types)
 
@@ -120,8 +126,7 @@ if (selected_node_types != st.session_state.prev_node_types or
     st.session_state.prev_relationship_types = selected_relationship_types
     # Construct and fetch new graph data
     cypher_query = construct_cypher_query(selected_node_types, selected_relationship_types)
-    with driver.session() as session:
-        nodes, edges = fetch_graph_data(session, cypher_query)
+    nodes, edges = fetch_graph_data(selected_node_types=selected_node_types, selected_relationship_types=selected_relationship_types)
     # Define the configuration for the graph visualization
     config = Config(height=600, width=800, directed=True, nodeHighlightBehavior=True, highlightColor="#F7A7A6")
     # Render the graph using agraph with the specified configuration
@@ -129,30 +134,30 @@ if (selected_node_types != st.session_state.prev_node_types or
 
 # Chat interface
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "Hi there, what can I help you with?"}]
+    st.session_state["messages"] = [{"role": "assistant", "content": "Hi there, ask me a question."}]
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
-if prompt := st.chat_input(placeholder="Type your query..."):
+if prompt := st.chat_input(placeholder="Ask a question"):
     if not openai_api_key:
         st.error("Please add your OpenAI API key to continue.")
     else:
-        user_id = '1'  # Example user ID
-        session_id = '1'  # Example session ID
-        # Process input through agent
-        from neo4j_advanced_rag.agent import AgentInput, agent_executor
-        response = agent_executor.invoke(AgentInput(input=prompt, user_id=user_id, session_id=session_id))
-        retriever_module = importlib.import_module("neo4j_advanced_rag.retrievers")
-        from neo4j_advanced_rag.neo4j_vector import VectorTool
-        graph_tool = GraphTool()
-        vector_tool = VectorTool(openai_api_key=openai_api_key)
-        # Now you can use functions or classes from retriever.py
-        # For example, retriever_module.some_function(openai_api_key)
-        cypher_query, nl_response = graph_tool._run(prompt, user_id, session_id)
         # Display response
-        st.write(response)
-
+        # Initialize the GraphCypherQAChain from chain.py
+        from chain import GraphCypherQAChain
+        cypher_chain = GraphCypherQAChain.from_llm(
+            cypher_llm=ChatOpenAI(temperature=0, model_name='gpt-4', api_key=openai_api_key),
+            qa_llm=ChatOpenAI(temperature=0, api_key=openai_api_key),
+            graph=graph,
+            verbose=True,
+            return_intermediate_steps=True
+)
         # Update session state with new message
         st.session_state.messages.append({"role": "user", "content": prompt})
+        st.chat_message("user").write(prompt)
+        response, nodes, edges= process_query(prompt)
+        config = Config(height=600, width=800, directed=True, nodeHighlightBehavior=True, highlightColor="#F7A7A6")
+        agraph(nodes=nodes, edges=edges, config=config)
         st.session_state.messages.append({"role": "assistant", "content": response})
+        st.chat_message("assistant").write(response)
